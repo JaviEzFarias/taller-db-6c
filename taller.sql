c0d723ce3c88e4650ef7ca601cf215176beeed28
JaviEzFarias <132085724+JaviEzFarias@users.noreply.github.com>
2023-05-18 12:55:03 -0300

    git add taller.sql

---
+++ taller.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-05-2023 a las 17:49:25
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `taller`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
+
+CREATE TABLE `asignatura` (
+  `cod-asignatura` varchar(200) NOT NULL,
+  `nombre` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `asignatura`
+--
+
+INSERT INTO `asignatura` (`cod-asignatura`, `nombre`) VALUES
+('1', 'Lengua'),
+('2', 'Educacion Artistica'),
+('3', 'Matematica'),
+('4', 'Educacion Tecnologica'),
+('5', 'Fisica'),
+('6', 'Ingles'),
+('7', 'Biologia'),
+('8', 'Ciudadania Y Participacion'),
+('9', 'Geografia'),
+('10', 'Dibujo Tecnico'),
+('11', 'Recreo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dias`
+--
+
+CREATE TABLE `dias` (
+  `cod-dia` varchar(200) NOT NULL,
+  `nombre` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `dias`
+--
+
+INSERT INTO `dias` (`cod-dia`, `nombre`) VALUES
+('1', 'Lunes'),
+('2', 'Martes'),
+('3', 'Miercoles'),
+('4', 'Jueves'),
+('5', 'Viernes');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horarios`
+--
+
+CREATE TABLE `horarios` (
+  `cod-hora` int(11) NOT NULL,
+  `cod-asignatura` int(11) NOT NULL,
+  `cod-dia` int(11) NOT NULL,
+  `hora-ingreso` varchar(5) NOT NULL,
+  `hora-egreso` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
